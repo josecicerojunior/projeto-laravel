7b562f26edf8307af380e463f71d917c9c84fa49
unknown <juniorgranjeiro.jc@gmail.com>
2024-10-17 18:35:25 -0300

    usando extends section yield

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="{{asset('css/estilo_basico.css')}}">
-
-    </head>
+@extends('site.layouts.basico')
+   
+@section('conteudo')
 
     <body>
         <div class="topo">
@@ -70,4 +65,4 @@
             </div>
         </div>
     </body>
-</html>
+    @endsection
