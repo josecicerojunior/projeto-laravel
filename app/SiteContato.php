01535078026eb8b734b956cbc4f339abfd0ddc96
unknown <juniorgranjeiro.jc@gmail.com>
2024-11-20 18:48:43 -0300

    adicionando softDeletes na tabela de fornecedores

--- app/SiteContato.php
+++ app/SiteContato.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
+
 
 class SiteContato extends Model
 {
+    use softDeletes;
     protected $table = 'site_contatos';
 }
