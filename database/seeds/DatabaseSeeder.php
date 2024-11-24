d6f3522c694047e8cfdb759af4481479847cdbcc
unknown <juniorgranjeiro.jc@gmail.com>
2024-11-23 23:41:06 -0300

    inserindo dados usando dbseed na tabela site contato

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(FornecedorSeeder::class);
+        $this->call(SiteContaoSeeder::class);
         
     }
 }
---
+++ database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SiteContato;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $contato = new SiteContato();
+        $contato->nome = 'Sistema SG';
+        $contato->telefone = '(62) 99999-9999';
+        $contato->email = 'contatos@sg.com.br';
+        $contato->motivo_contato = '1';
+        $contato->mensagem = 'Welcome to this app';
+        $contato->save();
+
+
+
+
+    }
+}
